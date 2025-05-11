4b66f9f543c761cdfb0b13ad791d6b49a21771f9
Syed Ijtaba Rizvi <i230112@isb.nu.edu.pk>
2025-05-11 16:04:02 +0500

    added manage events form added assign booth form (except add <assign booth query>)

--- WindowsFormsApp1/assign_booths.cs
+++ WindowsFormsApp1/assign_booths.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace WindowsFormsApp1
 {
     public partial class assign_booths : Form
     {
+        string connectionString = SessionData.ijtabastring;
         public assign_booths()
         {
             InitializeComponent();
@@ -58,5 +60,121 @@ namespace WindowsFormsApp1
             form1.Show();
             this.Hide();
         }
+        private void LoadEventNames()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT Title FROM JOB_FAIR_EVENTS", con); // Example
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    comboBox1.Items.Add(reader["Title"].ToString());
+                }
+            }
+        }
+
+        private void LoadCoordinators()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select name from users where role='Coordinator'", con); // Example
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    comboBox3.Items.Add(reader["Name"].ToString());
+                }
+            }
+        }
+
+        private void LoadCompanies()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT Name as [CompanyName] FROM COMPANY", con); // Example
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    comboBox2.Items.Add(reader["CompanyName"].ToString());
+                }
+            }
+        }
+        private void LoadBoothAssignments()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(
+                    @"SELECT e.title AS [Event Title], 
+                     u.Name AS [Coordinator], 
+                     com.Name AS [Company], 
+                     b.booth_id AS [Booth #]
+              FROM BOOTH_ASSIGNMENT b
+			  INNER JOIN BOOTH bo on bo.booth_ID=b.booth_ID
+              INNER JOIN JOB_FAIR_EVENTS e ON bo.EventId = e.eventid
+              INNER JOIN COMPANY com ON b.booth_id = com.company_Id
+              INNER JOIN BOOTH_TRACKING BR on b.booth_id=BR.booth_id
+              INNER JOIN USERS u ON BR.COORDINATOR_ID=u.User_id ", con); // Adjust fields/table names
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+            }
+        }
+
+        private void assign_booths_Load(object sender, EventArgs e)
+        {
+            LoadEventNames();
+            LoadCoordinators();
+            LoadCompanies();
+            LoadBoothAssignments(); // Load DataGridView
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //event name combo box 
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //coordinator combo box
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //company combo box
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string selectedEvent = comboBox1.SelectedItem?.ToString();
+            string selectedCoordinator = comboBox3.SelectedItem?.ToString();
+            string selectedCompany = comboBox2.SelectedItem?.ToString();
+            int tpoid = SessionData.UserId;
+
+            if (string.IsNullOrEmpty(selectedEvent) || string.IsNullOrEmpty(selectedCoordinator) || string.IsNullOrEmpty(selectedCompany))
+            {
+                MessageBox.Show("Please select all fields before assigning a booth.");
+                return;
+            }
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                //SqlCommand cmd = new SqlCommand("INSERT INTO BOOTH_ASSIGNMENTS (TPO_ID,EventName, CoordinatorName, CompanyName) VALUES (@tpo_id,@event, @coordinator, @company)", con); // Complete this
+                //cmd.Parameters.AddWithValue("@tpo_id", tpoid);
+                //cmd.Parameters.AddWithValue("@event", selectedEvent);
+                //cmd.Parameters.AddWithValue("@coordinator", selectedCoordinator);
+                //cmd.Parameters.AddWithValue("@company", selectedCompany);
+                //cmd.ExecuteNonQuery();
+                //MessageBox.Show("Booth assigned successfully!");
+
+                LoadBoothAssignments(); // Refresh DataGridView
+            }
+        }
+
     }
 }
